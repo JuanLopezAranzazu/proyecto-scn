cbad9f51af2a52874602d0b5e54277a02c11b1da
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2024-12-20 22:15:59 -0500

    Avances polinomio de taylor

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sympy as sp
+from utils.taylor_polynomial import taylor, absolute_error, relative_error, graph_taylor
 
 # funcion principal
 def main():
@@ -12,7 +14,27 @@ def main():
     option = input("Seleccione una opción: ")
 
     if option == "1":
-      print("Método polinomio de taylor")
+      # datos de entrada
+      f = input("Ingrese la función f(x): ")
+      x0 = float(input("Ingrese el punto de aproximación x0: "))
+      n = int(input("Ingrese el grado del polinomio de Taylor: "))
+      x = float(input("Ingrese el punto en el que se evalúa el error x: "))
+      x_range = int(input("Ingrese el rango de x: "))
+
+      # rango de x
+      x_range = (x0 - x_range, x0 + x_range)
+
+      # calcular el polinomio de Taylor
+      f = sp.sympify(f)
+      t = taylor(f, x0, n)
+      print(t)
+
+      # imprimir el error absoluto y relativo
+      print(f'Error absoluto: {absolute_error(f, t, x)}')
+      print(f'Error relativo: {relative_error(f, t, x)}')
+
+      # graficar la función y el polinomio de Taylor
+      graph_taylor(f, t, x0, x_range)
 
     elif option == "2":
       print("Método newton-raphson")
